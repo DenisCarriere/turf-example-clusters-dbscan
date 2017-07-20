3fe8c717d41040bc566f1a2a89d9ea242c6fb9ba
Denis <carriere.denis@gmail.com>
2017-07-20 18:10:21 -0400

    Update attribution

--- main.js
+++ main.js
@@ -8,9 +8,7 @@ const map = L.map('app').setView([45.414444, -75.697100], 12)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
-  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
   id: 'mapbox.streets'
 }).addTo(map)
 
